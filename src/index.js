77600dc021285ff9c2456277e4b618dfd03ef467
Duje Majic <duje.majic55@gmail.com>
2018-11-26 17:02:58 +0100

    initial commit of create-react-app + bootstrap

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles/index.scss';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
