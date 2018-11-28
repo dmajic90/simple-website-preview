ac6f9ddca18335c06605e006bde6446b3815a3ec
Duje Majic <duje.majic55@gmail.com>
2018-11-28 08:54:30 +0100

    added content and styling for it

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
+import Reviews from './reviews';
 
 export default class Content extends Component {
   constructor(props) {
@@ -9,21 +10,40 @@ export default class Content extends Component {
 
   render() {
     return (
-      <Container fluid='true'>
-        <section id='about' className='content-section'>
-          <div className='my-auto'>About</div>
+      <Container fluid={true}>
+        <section id='about' className='content-section d-flex d-column'>
+          <div className='content-wrapper my-auto text-left'>
+            <h1 className='content-title m-0'>
+              Fictional
+              <span className='content-title text-primary'> company</span>
+            </h1>
+            <div className='content-subhead mb-3 text-secondary'>
+              123 Fictional street &#x2027; Fictional city &#x2027; (123)
+              456-7890 &#x2027;
+              <a href='mailto:fictional@company.com'> fictional@company.com</a>
+            </div>
+            <div className='content-text text-secondary my-5'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              et ligula ligula. Maecenas dignissim felis eros, fermentum tempus
+              odio accumsan hendrerit. Nulla ornare est non lectus ultrices, sed
+              vulputate enim dictum. Cras vitae tellus non enim laoreet
+              fringilla et in massa.
+            </div>
+          </div>
         </section>
         <hr />
-        <section id='products' className='content-section'>
-          <div className='my-auto'>Products</div>
+        <section id='products' className='content-section d-flex d-column'>
+          <div className='my-auto text-left'>Products</div>
         </section>
         <hr />
-        <section id='reviews' className='content-section'>
-          <div className='my-auto'>Reviews</div>
+        <section id='reviews' className='content-section d-flex d-column'>
+          <div className='my-auto text-left'>
+            <Reviews />
+          </div>
         </section>
         <hr />
-        <section id='contact' className='content-section'>
-          <div className='my-auto'>Contact</div>
+        <section id='contact' className='content-section d-flex d-column'>
+          <div className='my-auto text-left'>Contact</div>
         </section>
         <hr />
       </Container>
