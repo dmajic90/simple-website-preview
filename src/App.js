c81bde7861e20a2bc34132fac24575b8a8c8b69e
Duje Majic <duje.majic55@gmail.com>
2018-11-30 00:09:26 +0100

    set provider and store for redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/index.js';
 import Navigation from './components/navigation';
 import Content from './components/content';
 import NoMatch from './components/NoMatch';
@@ -7,17 +9,19 @@ import NoMatch from './components/NoMatch';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/'>
-            <div>
-              <Navigation />
-              <Content />
-            </div>
-          </Route>
-          <Route component={NoMatch} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/'>
+              <div>
+                <Navigation />
+                <Content />
+              </div>
+            </Route>
+            <Route component={NoMatch} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
