19565995773afdabd8fa0ec538f27c86f9735588
Duje Majic <duje.majic55@gmail.com>
2018-11-30 18:16:03 +0100

    minor change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './store/index.js';
 import Navigation from './components/navigation';
 import Content from './components/content';
-import NoMatch from './components/NoMatch';
+import NoMatch from './components/noMatch';
 
 class App extends Component {
   render() {
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,1 @@
 export const SAVE_RESULTS = 'SAVE_RESULTS';
-export const RENDER_POST = 'RENDER_POST';
